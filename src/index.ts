b9e04fa94ffdfc63ed671bd86c8cf194c834b9e1
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2023-07-25 20:20:41 -0400

    Able to Make Pieces Move
    Was able to make it so that the pieces can move and merge with a simple left, down, and right pattern. Also created helpers such as visualizing the board, keeping track of the highest tile, and the number of moves

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import puppeteer, { KeyInput, Page } from "puppeteer";
 
-const hasChanged = async (page: Page): Promise<number[][]> => {
+const visualizeBoard = (board: number[][]): void => {
+  const grid = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  board.forEach((piece) => {
+    grid[piece[0]][piece[1]] = 1;
+  });
+
+  console.log(grid);
+};
+
+const getBoard = async (
+  page: Page,
+  highestTile: number
+): Promise<[number[][], number]> => {
   const newBoard = [];
 
   await page.waitForSelector(".tile-container");
@@ -14,16 +32,28 @@ const hasChanged = async (page: Page): Promise<number[][]> => {
       const className = (await child.getProperty("className")).toString();
       if (!className.includes("tile-inner")) {
         console.log(className + "\n");
-        const positionClass = className.split(" ")[2];
+        const splitClasses = className.split(" ");
+        const positionClass = splitClasses[2];
         console.log(positionClass + "\n");
         const splitPosition = positionClass.split("-");
         console.log(splitPosition + "\n");
         newBoard.push([parseInt(splitPosition[2]), parseInt(splitPosition[3])]);
+        const tileValue = parseInt(splitClasses[1].split("-")[1]);
+        if (tileValue > highestTile) {
+          highestTile = tileValue;
+        }
       }
     }
   }
 
-  return newBoard;
+  return [newBoard, highestTile];
+};
+
+const isGameOver = async (page: Page): Promise<boolean> => {
+  const gameOver = await page.evaluate(() => {
+    return !!document.querySelector(".game-over"); // !! converts anything to boolean
+  });
+  return gameOver;
 };
 
 (async () => {
@@ -34,29 +64,74 @@ const hasChanged = async (page: Page): Promise<number[][]> => {
   // Navigate the page to a URL
   await page.goto("https://play2048.co/");
 
-  const moveSequence: KeyInput[] = ["ArrowDown", "ArrowLeft", "ArrowRight"];
+  // const moveSequence: KeyInput[] = ["ArrowDown", "ArrowLeft", "ArrowRight"];
+  const moveSequence: KeyInput[] = ["ArrowLeft", "ArrowDown", "ArrowRight"];
 
   let moveIndex = 0;
 
-  let board: number[][] = [[], [], []];
+  let board: number[][] = [[], [], [], []];
 
-  let newBoard: number[][] = [[], [], []];
+  let newBoard: number[][] = [[], [], [], []];
 
-  for (let i = 0; i < 10; i++) {
-    newBoard = await hasChanged(page);
-    if (board !== newBoard) {
-      continue;
-    } else {
-      moveIndex += 1;
+  let repeats = 0;
+
+  let loops = 0;
+
+  let moves = 0;
+
+  let highestTile = 0;
+
+  for (let i = 0; i < 500; i++) {
+    const gameOver = await isGameOver(page);
+    if (gameOver) {
+      console.log("GAME OVER!");
+      break;
     }
+    const data = await getBoard(page, highestTile);
+    newBoard = data[0];
+    highestTile = data[1];
+    console.log("Prev Board: ", board);
+    console.log("New Board: ", newBoard);
+    // if (JSON.stringify(board) == JSON.stringify(newBoard)) {
+    //   console.log("Board has not changed");
+    //   repeats += 1;
+    //   if (repeats < 2) {
+    //     if (moveIndex == 0) {
+    //       moveIndex = 1;
+    //     } else if (moveIndex == 1) {
+    //       moveIndex = 0;
+    //     } else {
+    //       moveIndex = 0;
+    //     }
+    //   } else {
+    //     if (moveIndex == 2) {
+    //       moveIndex = 0;
+    //     } else {
+    //       moveIndex++;
+    //     }
+    //   }
+    // } else {
+    //   moves++;
+    //   repeats = 0;
+    // }
     board = newBoard;
-    console.log("Board: ", board);
-    console.log("Pressing: " + moveSequence[0]);
-    await page.keyboard.press(moveSequence[0]);
+    console.log("\nMove Index", moveIndex);
+    console.log("Pressing: " + moveSequence[moveIndex], "\n");
+    // loops++;
+    moves++;
+    if (moveIndex < 2) {
+      moveIndex++;
+    } else {
+      moveIndex = 0;
+    }
+    await page.keyboard.press(moveSequence[moveIndex]);
   }
+
+  console.log("Moves: ", moves);
+  console.log("Highest Tile: ", highestTile);
 
   setTimeout(async () => {
     console.log("Finished");
     await browser.close();
-  }, 10000);
+  }, 4000);
 })();
